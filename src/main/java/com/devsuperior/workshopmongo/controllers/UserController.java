ce6beba084422809bb054a2faea24303b2d7599e
Jacques-Trevia <jacquesceara@gmail.com>
2025-12-18 21:06:53 -0300

    POST  new user

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -3,10 +3,15 @@ package com.devsuperior.workshopmongo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
+import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.services.UserService;
 
 import reactor.core.publisher.Flux;
@@ -25,7 +30,15 @@ public class UserController {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public Mono<ResponseEntity<UserDTO>> findById(String id) {
+	public Mono<ResponseEntity<UserDTO>> findById(@PathVariable String id) {
 		return service.findById(id).map(userDTO -> ResponseEntity.ok().body(userDTO));
+	}
+	
+	@PostMapping
+	public Mono<ResponseEntity<UserDTO>> insert(@RequestBody UserDTO dto, UriComponentsBuilder builder){
+		return service.insert(dto).map(newUser -> ResponseEntity
+				.created(builder.path("/users/{id}")
+						.buildAndExpand(newUser.getId())
+						.toUri()).body(newUser));
 	}
 }
