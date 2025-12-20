3cf882ace595b473b1c6cb5149288f13d7483549
Jacques-Trevia <jacquesceara@gmail.com>
2025-12-19 21:23:53 -0300

    GET user posts

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -49,4 +49,9 @@ public class PostController {
 		
 	    return service.fullsearch(text, min, max);
 	}
+	
+	@GetMapping(value = "/user/{id}")
+	public Flux<PostDTO> findByUser(@PathVariable String id){
+		return service.findByUser(id);
+	}
 }
